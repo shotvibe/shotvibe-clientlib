45becb6797e67832a4b10dec3bd78748af09dfbf
benny <benny@shotvibe.com>
2014-01-16 07:08:12 -0800

    Add some more standard constructors SQLException

--- src/main/java/com/shotvibe/shotvibelib/SQLException.java
+++ src/main/java/com/shotvibe/shotvibelib/SQLException.java
@@ -1,4 +1,19 @@
 package com.shotvibe.shotvibelib;
 
 public class SQLException extends Exception {
+    public SQLException() {
+        super();
+    }
+
+    public SQLException(String detailMessage) {
+        super(detailMessage);
+    }
+
+    public SQLException(String detailMessage, Throwable throwable) {
+        super(detailMessage, throwable);
+    }
+
+    public SQLException(Throwable throwable) {
+        super(throwable);
+    }
 }
