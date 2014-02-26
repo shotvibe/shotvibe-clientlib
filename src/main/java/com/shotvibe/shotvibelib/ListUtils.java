bb94eba441e26ca2bae63dea26022d034a1c52c8
benny <benny@shotvibe.com>
2014-02-26 06:25:27 -0800

    WIP Add ListUtils class

---
+++ src/main/java/com/shotvibe/shotvibelib/ListUtils.java
@@ -0,0 +1,28 @@
+package com.shotvibe.shotvibelib;
+
+/*
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+*/
+
+public final class ListUtils {
+/*
+    public interface Comparator<T> {
+        int compare(T object1, T object2);
+    }
+
+    public static <T> void sortList(List<T> list, Comparator<? super T> c) {
+        final Comparator<? super T> c2 = c;
+        Collections.sort(list, new java.util.Comparator<T>() {
+            @Override
+            public int compare(T lhs, T rhs) {
+                return c2.compare(lhs, rhs);
+            }
+        });
+    }
+*/
+    private ListUtils() {
+        // Not used
+    }
+}
