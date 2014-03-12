7471961bb13fb38e9a8e8a65206141fcb5d31776
Oblosys <martijn@oblomov.com>
2014-03-12 13:00:47 +0100

    Bump database version to 3
    There were breaking changes in the database scheme

--- src/main/java/com/shotvibe/shotvibelib/ShotVibeDB.java
+++ src/main/java/com/shotvibe/shotvibelib/ShotVibeDB.java
@@ -8,7 +8,7 @@ public final class ShotVibeDB {
         mConn = conn;
     }
 
-    public static final int DATABASE_VERSION = 2;
+    public static final int DATABASE_VERSION = 3;
 
     /**
      * Should be called after populateNewDB or upgradeDB has been called (if one of them was necessary)
