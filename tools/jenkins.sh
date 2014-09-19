0b5827a7c548e2f8dd8915219edd1af5263940f6
benny <benny@shotvibe.com>
2014-09-19 19:52:27 +0300

    Check the style of the Obj-C source files

--- tools/jenkins.sh
+++ tools/jenkins.sh
@@ -11,5 +11,8 @@ rm -f reports/checkstyle-result.xml
 
 find src -name '*.java' -print0 | xargs -0 java -classpath src/main/java:third_party/jars/checkstyle-5.7-all.jar com.puppycrawl.tools.checkstyle.Main -c checkstyle.xml -f xml -o reports/checkstyle-result.xml || true
 
+# Run a style check on the Obj-C source files
+./tools/objc_style_check.sh > reports/style_check.report || true
+
 # Test the Obj-C Build
 ./tools/test_objc_build.sh
