997642e4e3593ed1561116dc2dccdf3313ba3a94
benny <benny@shotvibe.com>
2014-12-09 13:40:03 +0200

    Bring more notification types into NotificationMessage

--- src/main/java/com/shotvibe/shotvibelib/NotificationMessage.java
+++ src/main/java/com/shotvibe/shotvibelib/NotificationMessage.java
@@ -2,6 +2,10 @@ package com.shotvibe.shotvibelib;
 
 public abstract class NotificationMessage {
     public interface NotificationHandler {
+        void Handle(NotificationMessage.TestMessage msg);
+        void Handle(NotificationMessage.AlbumSync msg);
+        void Handle(NotificationMessage.PhotosAdded msg);
+        void Handle(NotificationMessage.AddedToAlbum msg);
         void Handle(NotificationMessage.PhotoGlance msg);
     }
 
@@ -21,13 +25,146 @@ public abstract class NotificationMessage {
         String type;
         try {
             type = msg.getString("type");
-            if (type.equals("photo_glance")) {
+            if (type.equals("test_message")) {
+                return TestMessage.parse(msg);
+            } else if (type.equals("album_sync")) {
+                return AlbumSync.parse(msg);
+            } else if (type.equals("photos_added")) {
+                return PhotosAdded.parse(msg);
+            } else if (type.equals("added_to_album")) {
+                return AddedToAlbum.parse(msg);
+            } else if (type.equals("photo_glance")) {
                 return PhotoGlance.parse(msg);
             } else {
                 throw new ParseException("Unknown Message type: " + type);
             }
         } catch (JSONException e) {
             throw ParseException.FromJSONException(e);
+        }
+    }
+
+    public static final class TestMessage extends NotificationMessage {
+        public static TestMessage parse(JSONObject msg) throws ParseException, JSONException {
+            String message = msg.getString("message");
+            return new TestMessage(message);
+        }
+
+        private TestMessage(String message) {
+            mMessage = message;
+        }
+
+        public String getMessage() {
+            return mMessage;
+        }
+
+        private final String mMessage;
+
+        @Override
+        public void handle(NotificationHandler handler) {
+            handler.Handle(this);
+        }
+    }
+
+    public static final class AlbumSync extends NotificationMessage {
+        public static AlbumSync parse(JSONObject msg) throws ParseException, JSONException {
+            long album_id = msg.getLong("album_id");
+            return new AlbumSync(album_id);
+        }
+
+        private AlbumSync(long albumId) {
+            mAlbumId = albumId;
+        }
+
+        public long getAlbumId() {
+            return mAlbumId;
+        }
+
+        private final long mAlbumId;
+
+        @Override
+        public void handle(NotificationHandler handler) {
+            handler.Handle(this);
+        }
+    }
+
+    public static final class PhotosAdded extends NotificationMessage {
+        public static PhotosAdded parse(JSONObject msg) throws ParseException, JSONException {
+            long album_id = msg.getLong("album_id");
+            String author_name = msg.getString("author");
+            String album_name = msg.getString("album_name");
+            int num_photos = msg.getInt("num_photos");
+
+            return new PhotosAdded(album_id, author_name, album_name, num_photos);
+        }
+
+        private PhotosAdded(long albumId, String authorName, String albumName, int numPhotos) {
+            mAlbumId = albumId;
+            mAuthorName = authorName;
+            mAlbumName = albumName;
+            mNumPhotos = numPhotos;
+        }
+
+        public long getAlbumId() {
+            return mAlbumId;
+        }
+
+        public String getAuthorName() {
+            return mAuthorName;
+        }
+
+        public String getAlbumName() {
+            return mAlbumName;
+        }
+
+        public int getNumPhotos() {
+            return mNumPhotos;
+        }
+
+        private final long mAlbumId;
+        private final String mAuthorName;
+        private final String mAlbumName;
+        private final int mNumPhotos;
+
+        @Override
+        public void handle(NotificationHandler handler) {
+            handler.Handle(this);
+        }
+    }
+
+    public static final class AddedToAlbum extends NotificationMessage {
+        public static AddedToAlbum parse(JSONObject msg) throws ParseException, JSONException {
+            long album_id = msg.getLong("album_id");
+            String adder_name = msg.getString("adder");
+            String album_name = msg.getString("album_name");
+
+            return new AddedToAlbum(album_id, adder_name, album_name);
+        }
+
+        private AddedToAlbum(long albumId, String adderName, String albumName) {
+            mAlbumId = albumId;
+            mAdderName = adderName;
+            mAlbumName = albumName;
+        }
+
+        public long getAlbumId() {
+            return mAlbumId;
+        }
+
+        public String getAdderName() {
+            return mAdderName;
+        }
+
+        public String getAlbumName() {
+            return mAlbumName;
+        }
+
+        private final long mAlbumId;
+        private final String mAdderName;
+        private final String mAlbumName;
+
+        @Override
+        public void handle(NotificationHandler handler) {
+            handler.Handle(this);
         }
     }
 
